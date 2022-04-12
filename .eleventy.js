1e1a6f905c4022ce72bbda17d320f1513b7320cd
Thomas Seaver <thomasseaver98@gmail.com>
2022-04-11 22:02:43 -0400

    Current draft of site

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,5 @@
-const CleanCSS = require("clean-css");
 module.exports = (config) => {
-  config.addFilter("cssmin", (code) => new CleanCSS({}).minify(code).styles);
-
-  return {
-    dir: {
-      includes: "includes",
-      input: "src",
-      output: "docs",
-    },
-  };
+  config.addPassthroughCopy("assets");
+  config.addPassthroughCopy("css");
+  return;
 };
